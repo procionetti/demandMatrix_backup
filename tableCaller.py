525bc290ca354e956ffdce22e93a5712fdfc4a5d
procionetti <matteo.canaletti@gmail.com>
2024-09-25 11:54:09 +0200

    Added region as choice and convert from Column to Row layout

--- tableCaller.py
+++ tableCaller.py
@@ -22,16 +22,14 @@ import plotly.express as px
 import json
 import pyogrio
 
-from bokeh.io import output_notebook, show, output_file
+from bokeh.io       import output_notebook, show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, RadioButtonGroup,Slider
+from bokeh.models   import GeoJSONDataSource, LinearColorMapper, ColorBar, RadioButtonGroup,Slider
 from bokeh.palettes import brewer
 
 from bokeh.io.doc import curdoc
 from bokeh.layouts import widgetbox, row, column
 
-regionCode = int(input("Insert 1 for FLGV and 2 for Twente:_____"))
-regios_dict = {1:"FGV",2:"Twente"}
 
 # 0) Call saved hexagons as geodataframes
 #hexagons_file_path = f'saved_tables/{regios_dict[regionCode]}Hexagons.json'  
@@ -41,27 +39,32 @@ regios_dict = {1:"FGV",2:"Twente"}
 #geojson_data1 = json.loads(geojson_string2)
 #GDFhexagons = gpd.GeoDataFrame.from_features(geojson_data1["features"])
 
-# 1) Call saved geotables as geodataframes
-hexgrid1 = gpd.read_file(f'saved_tables/hexgrid{regios_dict[regionCode]}_2122_A1.geojson')
-hexgrid2 = gpd.read_file(f'saved_tables/hexgrid{regios_dict[regionCode]}_2122_A2.geojson')
-hexgrids = [hexgrid1,hexgrid2]
+# 1) Define Regions dictionary and Call saved geotables as geodataframes
+#regionCode = int(input("Insert 1 for FLGV and 2 for Twente:_____"))
+regios_dict = {1:"FGV",2:"Twente"}
 
-# 8) Define a sequential multi-hue color palette and reverse color order so that dark blue is highest obesity.
+#hexgrid1 = gpd.read_file(f'saved_tables/hexgrid{regios_dict[regionCode]}_2122_A1.geojson')
+#hexgrid2 = gpd.read_file(f'saved_tables/hexgrid{regios_dict[regionCode]}_2122_A2.geojson')
+#hexgrids = [hexgrid1,hexgrid2]
+
+# 2) Define date labels and sequential multi-hue color palette and reverse color order so that dark blue is highest obesity.
 Day_Labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 Month_Labels= ["January","February","March","April","May","June","July","August","September","October","November","December"]
 palette = brewer['Blues'][8]
 palette = palette[::-1]
 
-# 9) Function that returns json_data for the month+weekday+urgency selected 
-def json_data(selectedMonth,selectedDay,selectedUrgency):
+# 3) Function that returns json_data for the region+month+weekday+urgency selected 
+def json_data(selectedRegion,selectedMonth,selectedDay,selectedUrgency):
     # convert from A 1/2 to 0/1 index
-    ur = selectedUrgency-1
+    # ur = selectedUrgency-1    
+    # Pick region's table using selectedRegion and urgency
+    urg_gdf = gpd.read_file(f'saved_tables/hexgrid{regios_dict[selectedRegion]}_2122_A{selectedUrgency}.geojson')
     # Select urgencyGrid 
-    urg_gdf = hexgrids[int(ur)]
+    # urg_gdf = hexgrids[int(ur)]
     # Pull selected month+day from geodataframe into df
     gdf_mn_day = urg_gdf.filter(like=f"month:{selectedMonth}:-day:{selectedDay}")
     # Merge the hexagons (gdf) with the data (gdf_mn_day)
-    merged =  gpd.GeoDataFrame(pd.concat([gpd.GeoDataFrame(hexgrid1.geometry),gdf_mn_day],axis=1))
+    merged =  gpd.GeoDataFrame(pd.concat([gpd.GeoDataFrame(urg_gdf.geometry),gdf_mn_day],axis=1))
     # Bokeh uses geojson formatting, representing geographical features, with json
     mergedToLoad=merged.to_json()
     # Convert to json
@@ -70,33 +73,31 @@ def json_data(selectedMonth,selectedDay,selectedUrgency):
     json_data = json.dumps(merged_json)
     return json_data
 # Input geojson source that contains features for starting plot when opening application
-geosource = GeoJSONDataSource(geojson = json_data(1,1,1))
+geosource = GeoJSONDataSource(geojson = json_data(1,1,1,1))
 
-# 10) Define the callback function: update_plot which obv updates plots with values selcted on applet
+# 4) Define the callback function: update_plot which obv updates plots with values selcted on applet
 def update_plot(attr, old, new):
-
-    month = mon_slider.value
-    #day = day_slider.value
-    # Replace slider with RadGroup
-    day = rad_group.active + 1
-    urgency = urg_slider.value
-    new_data = json_data(month,day,urgency)        
+    region   = rad_regio.active + 1
+    month    = mon_slider.value
+    day      = rad_group.active + 1
+    urgency  = urg_slider.value
+    new_data = json_data(region,month,day,urgency)        
     # Update the plot based on the changed inputs
-    p = make_plot(month,day,urgency)
+    p = make_plot(region,month,day,urgency)
     # Update the layout, clear the old document and display the new document
-    sliders = column(mon_slider,urg_slider,rad_group)
-    layout = column(p, sliders)
+    sliders = column(rad_regio,mon_slider,urg_slider,rad_group)
+    layout = row(p, sliders)
     curdoc().clear()
     curdoc().add_root(layout)
     # Update the data
     geosource.geojson = new_data
 
-# 11) Create a plotting function which defines what the plot looks like 
-def make_plot(month,day,urgency):    
+# 5) Create a plotting function which defines what the plot looks like 
+def make_plot(region,month,day,urgency):    
   
   date=f"month:{month}:-day:{day}"
-  urg = urgency - 1
-  urg_gdf = hexgrids[int(urg)]
+  # urg = urgency - 1
+  urg_gdf = gpd.read_file(f'saved_tables/hexgrid{regios_dict[region]}_2122_A{urgency}.geojson')
   # Set the format of the colorbar
   min_range = np.array(urg_gdf.filter(like=f"month:{month}:-day:{day}").min())[0]
   max_range = np.array(urg_gdf.filter(like=f"month:{month}:-day:{day}").max())[0]
@@ -106,7 +107,7 @@ def make_plot(month,day,urgency):
   color_bar = ColorBar(color_mapper=color_mapper, label_standoff=18,border_line_color=None, location = (0, 0))
 
   # Create figure object.
-  p = figure(title = f"Average number of A{urgency} incidents on {Day_Labels[day-1]}\'s of {Month_Labels[month-1]}", 
+  p = figure(title = f"Average number of A{urgency} incidents on {Day_Labels[day-1]}\'s of {Month_Labels[month-1]} in {regios_dict[region]}", 
             plot_height = 650, plot_width = 850,
             toolbar_location = None)
   p.xgrid.grid_line_color = None
@@ -119,22 +120,26 @@ def make_plot(month,day,urgency):
   p.add_layout(color_bar, 'right')
   return p
 
-# 12) Call the plotting function 
-p = make_plot(1,1,1)
+# 6) Call the plotting function 
+p = make_plot(1,1,1,1)
 
-# 13) Add checkbox group for weekdays(trial). 
+# 7) Add checkbox group for weekdays. 
 rad_group = RadioButtonGroup(labels=Day_Labels, active=0)
 rad_group.on_change('active', update_plot) # rad_group returns [i,j] if i,j clicked, otherwise [].
 
+# 8) Add checkbox group for regions (trial). 
+rad_regio = RadioButtonGroup(labels=list(regios_dict.values()), active=0)
+rad_regio.on_change('active', update_plot)
 # Make a MONTHS slider object 
 mon_slider = Slider(title = 'Month',start = 1, end = 12, step = 1, value = 1)
 mon_slider.on_change('value', update_plot)
 # Make a URGENCY slider object 
 urg_slider = Slider(title = 'Urgency A',start = 1, end = 2, step = 1, value = 1)
 urg_slider.on_change('value', update_plot)
-# Make a column layout of widgetbox(slider) and plot, and add it to the current document
+
+# 9) Make a column layout of widgetbox(slider) and plot, and add it to the current document
 # Display the current document
 sliders = column(mon_slider,urg_slider,rad_group)
-layout = column(p,sliders)
+layout = row(p,sliders)
 #layout = column(p, widgetbox(mon_slider), widgetbox(day_slider), widgetbox(urg_slider))
 curdoc().add_root(layout)
